f4ed6e2826c84a39c8773b7e8e743ab92869149c
PMagombedze <percymagoras@outlook.com>
2024-01-27 16:34:49 +0200

    pascal_

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """
 Pascal's tri
@@ -12,7 +12,6 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    
     triangle = [[1]]
     for i in range(1, n):
         row = [1]
@@ -21,5 +20,5 @@ def pascal_triangle(n):
             row.append(last_row[j - 1] + last_row[j])
         row.append(1)
         triangle.append(row)
-    
+
     return triangle
