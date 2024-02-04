c15cc9297d9bfeed9c2738afe338bea1049685bb
PMagombedze <percymagoras@outlook.com>
2024-02-04 18:13:35 +0200

    min ops

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,35 +6,15 @@ minimum operations
 """
 
 
-import math
-
-
-def is_prime(n):
-    max_ = math.floor(math.sqrt(n))
-    for x in range(2, max_+1):
-        if n % x == 0:
-            return 'not prime'
-    return 'prime'
-
-
-def minOperations(n: int) -> int:
-    """min ops"""
+def minOperations(n):
     if n <= 0:
         return 0
-    elif n < 6:
-        return n
-    elif is_prime(n) is True:
-        return n
-    else:
-        lst = []
-        even = 5
-        for x in range(6, n):
-            if is_prime(x) == 'not prime':
-                lst.append(x)
-            else:
-                pass
-        for i in lst:
-            if i % 2 == 0:
-                c = i - 6
-                d = c/2
-        return int(d + even)
+    ops = 0
+    factor = 2
+    while n > 1:
+        while n % factor == 0:
+            ops += factor
+            n /= factor
+        factor += 1
+
+    return ops
