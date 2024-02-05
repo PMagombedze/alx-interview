753fb35c121e1e00363e9cc65f719f9a81f98a1b
PMagombedze <percymagoras@outlook.com>
2024-02-05 21:14:29 +0200

    utf8_

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,7 +8,20 @@ utf validation
 
 def validUTF8(data):
     """valid utf8"""
+    count = 0
     for x in data:
-        if x > 127 or x < 0:
-            return False
-    return True
+        BIT = format(x, '#010b')[-8:]
+        if count == 0:
+            for bit in BIT:
+                if bit == '0':
+                    break
+                count += 1
+            if count == 0:
+                continue
+            if count == 1 or count > 4:
+                return False
+        else:
+            if not (BIT[0] == '1' and BIT[1] == '0'):
+                return False
+        count -= 1
+    return count == 0
