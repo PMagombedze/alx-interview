bd33b4e610239d343d33f9099cadf3ab2b2d45dc
PMagombedze <percymagoras@outlook.com>
2024-02-25 20:34:21 +0200

    matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
 
+def transpose(matrix):
+    """matrix transpose"""
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
 def rotate_2d_matrix(matrix):
     """
     rotate 2d matrix clockwise
@@ -11,9 +18,6 @@ def rotate_2d_matrix(matrix):
     Returns:
         None: The matrix is modified in-place.
     """
-    n = len(matrix)
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    transpose(matrix)
     for x in matrix:
         x.reverse()
