d26d4a222cd9269a74dd3559a6a8cc857a1e3b30
PMagombedze <percymagoras@outlook.com>
2024-02-25 18:40:04 +0200

    document 2d clockwise

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,7 +2,15 @@
 
 
 def rotate_2d_matrix(matrix):
-    """rotate 2d matrix clockwise"""
+    """
+    rotate 2d matrix clockwise
+
+    Args:
+        matrix (List[List[int]]): The input matrix.
+
+    Returns:
+        None: The matrix is modified in-place.
+    """
     n = len(matrix)
     for i in range(n):
         for j in range(i, n):
