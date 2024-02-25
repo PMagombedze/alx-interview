ba8313e83bc14a65206578ccc3158ca0c34b3f53
PMagombedze <percymagoras@outlook.com>
2024-02-25 18:33:45 +0200

    2d clockwise

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,5 +2,10 @@
 
 
 def rotate_2d_matrix(matrix):
-    """rotate clockwise"""
-    pass
+    """rotate 2d matrix clockwise"""
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for x in matrix:
+        x.reverse()
