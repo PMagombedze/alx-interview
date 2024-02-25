79d921af1ace81edd0e008887fa4a43b59181133
PMagombedze <percymagoras@outlook.com>
2024-02-25 20:43:28 +0200

    pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,12 +3,14 @@
 2d matrix rotation
 """
 
+
 def transpose(matrix):
     """matrix transpose"""
     n = len(matrix)
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
 
 def rotate_2d_matrix(matrix):
     """
