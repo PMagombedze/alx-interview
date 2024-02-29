90752c85b1110c879db1b6334f8bc5a537434cdd
PMagombedze <percymagoras@outlook.com>
2024-02-29 19:42:57 +0200

    coin change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+"""
+coin problem
+"""
+
+
+def makeChange(coins, total):
+    """make change"""
+    num = float('inf')
+    solutionSet = num * (total + 1)
+    solutionSet[0] = 0
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            solutionSet[amount] = min(
+                solutionSet[amount], solutionSet[amount - coin] + 1
+            )
+    if solutionSet[total] == num:
+        return -1
+    else:
+        return solutionSet[total]
