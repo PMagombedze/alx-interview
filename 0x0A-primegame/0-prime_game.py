5524e613e3e9202564266af4c69b956be30045a6
PMagombedze <percymagoras@outlook.com>
2024-03-15 01:10:55 +0200

    update prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,37 +2,46 @@
 
 
 """
-Prime game
+prime game
 """
 
 
+def isPrime(num):
+    """Check if a number is prime."""
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
 def isWinner(x, nums):
-    """check for winner: ben/maria"""
-
-    def canWin(num):
-        """determine if can win"""
-        if num == 1:
-            return False
-        if num == 2:
-            return True
-        if num % 2 == 0:
-            return False
-        return True
-
+    """determine winner"""
     countMaria = 0
     countBen = 0
 
     for n in nums:
-        determineWin = canWin(n)
-        if determineWin:
-            if n % 2 == 0:
-                countMaria += 1
-            else:
-                countBen += 1
+        numbers = list(range(1, n + 1))
+        maria_turn = True
+        while numbers:
+            prime = None
+            for num in numbers:
+                if isPrime(num):
+                    prime = num
+                    break
+            if prime is None:
+                if maria_turn:
+                    countBen += 1
+                else:
+                    countMaria += 1
+                break
+            numbers = [num for num in numbers if num % prime != 0]
+            maria_turn = not maria_turn
 
     if countMaria > countBen:
         return "Maria"
     elif countBen > countMaria:
         return "Ben"
     else:
-        return "None"
+        return None
