6afb5500cdfb8724a43be8518ebcb29066abd52c
PMagombedze <percymagoras@outlook.com>
2024-03-25 20:33:34 +0200

    prime game update

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,38 +6,24 @@ prime game
 """
 
 
-def sieve_of_eratosthenes(n):
-    """Sieve of Eratosthenes algorithm to find all primes less than n."""
-    prime = [True for _ in range(n+1)]
-    p = 2
-    while (p * p <= n):
-        p_ = prime[p]
-        if p_ is True:
-            for i in range(p * p, n+1, p):
-                prime[i] = False
-        p += 1
-    primes = [p for p in range(2, n+1) if prime[p]]
-    return primes
-
-
 def isWinner(x, nums):
-    """Determine the winner of the prime game."""
-    maria_wins = 0
-    ben_wins = 0
-    for n in nums:
-        primes = sieve_of_eratosthenes(n)
-        turn = 'Maria'
-        while primes:
-            prime = primes.pop(0)
-            primes = [num for num in primes if num % prime != 0]
-            turn = 'Ben' if turn == 'Maria' else 'Maria'
-        if turn == 'Maria':
-            ben_wins += 1
-        else:
-            maria_wins += 1
-    if maria_wins > ben_wins:
-        return 'Maria'
-    elif ben_wins > maria_wins:
-        return 'Ben'
-    else:
+    """
+    Evaluates the winner of a prime game
+    """
+    if x < 1 or not nums:
         return None
+    mariasWins, bensWins = 0, 0
+    n = max(nums)
+    primes = [True] * n
+    primes[0] = False
+    for i in range(2, int(n ** 0.5) + 1):
+        if primes[i - 1]:
+            for j in range(i * i, n + 1, i):
+                primes[j - 1] = False
+    for n in nums:
+        primesCount = sum(1 for prime in primes[:n] if prime)
+        bensWins += primesCount % 2 == 0
+        mariasWins += primesCount % 2 == 1
+    if mariasWins == bensWins:
+        return None
+    return 'Maria' if mariasWins > bensWins else 'Ben'
